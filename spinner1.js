62bbabad40ce9e7989f3ad3efaad2e6d0013e4e5
elyeel <elyeel@yahoo.com>
2019-07-30 22:18:22 +0000

    first spinner function by: Peter & Ris

---
+++ spinner1.js
@@ -0,0 +1,27 @@
+// // process.stdout.write('hello from spinner1.js... \rheyyy\n');
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+let word = "|/-\|/-\||/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\       ";
+let time = 1000;
+for (const char of word) {
+  setTimeout(() => {
+    process.stdout.write("\r");
+    process.stdout.write(char);
+  }, time);
+  time += 250;
+};
